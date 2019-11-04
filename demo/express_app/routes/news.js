498478e1163c8dcb0911c04ca7578c593288e526
ydzhony1 <ydzhony1@163.com>
2019-11-04 17:09:51 +0800

    update pug

--- demo/express_app/routes/news.js
+++ demo/express_app/routes/news.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('news', { title: '新闻中心' });
+ // res.send('news22222222 <a href="/">返回首页</>');
 });
 
 module.exports = router;
