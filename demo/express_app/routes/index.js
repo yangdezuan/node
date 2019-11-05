882febbe79c7f927870e17bfd85ce8068d76e86a
ydzhony1 <ydzhony1@163.com>
2019-11-05 18:37:00 +0800

    update

--- demo/express_app/routes/index.js
+++ demo/express_app/routes/index.js
@@ -1,36 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-//暴露接口
-router.get('/', (req, res, next) => {
-  const { username, password } = req.query;
-  res.render('categorys', {
-    categorys: JSON.stringify([
-      {
-        id: 1001,
-        content: '商品一',
-        username,
-        password
-      },
-      {
-        id: 1002,
-        content: '商品二'
-      }
-    ])
-  });
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
-//暴露接口
-router.post('/', (req, res, next) => {
-  const { username, password } = req.body;
-  res.render('categorys', {
-    categorys: JSON.stringify([
-      {
-        id: 1003,
-        content: '商品三',
-        status: 1
-      }
-    ])
-  });
-});
 module.exports = router;
