d74ebc466b6515309f536225d57781fc6fb27f13
ydzhony1 <ydzhony1@163.com>
2019-11-05 18:22:42 +0800

    init ejs

--- demo/express_app/routes/users.js
+++ demo/express_app/routes/users.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
- //res.send('respond with a resource');
-  res.render('users', { title: '用户中心' });
+  res.send('respond with a resource');
 });
 
 module.exports = router;
